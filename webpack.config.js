5a7ea6dbec4ecfea613c447a769da6482fc4e64e
juicejun <827387950@qq.com>
2020-11-16 20:14:58 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
-const path = require('path');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode:'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'juice jun',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
+
